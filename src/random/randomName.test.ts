2f0d69ef09a3bfa60b9c826b4a5c98f533a59715
Orlando <orlando.groppo@gmail.com>
2023-03-17 16:42:28 +0000

    failing test

--- src/random/randomName.test.ts
+++ src/random/randomName.test.ts
@@ -1,13 +1,8 @@
 import { describe, it, expect } from "@jest/globals";
-import { randomArrayItem } from "./randomArrayItem";
-import { randomHandle, randomName } from "./randomName";
+import { randomName } from "./randomName";
 
 describe(`randomName`, () => {
   it(`randomName`, () => {
-    expect(randomName()).toBeDefined();
-  });
-
-  it(`randomHandle`, () => {
-    expect(randomHandle()).toBeDefined();
+    expect(randomName().length).toBeGreaterThan(0);
   });
 });
