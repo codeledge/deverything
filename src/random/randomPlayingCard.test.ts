5197c612696fb1f8a5d43279d73ab56736320099
Orlando <orlando.groppo@gmail.com>
2023-07-19 12:42:29 +0100

    for some reason

--- src/random/randomPlayingCard.test.ts
+++ src/random/randomPlayingCard.test.ts
@@ -3,7 +3,6 @@ import { randomBlackJackDeck } from "./randomPlayingCard";
 
 describe(`randomParagraph`, () => {
   it(`no args`, () => {
-    console.log(randomBlackJackDeck());
     expect(randomBlackJackDeck().length).toBe(52);
   });
 });
