35236c73b1a42158e583ec69eb51374c7b0242d4
Orlando <orlando.groppo@gmail.com>
2023-08-14 21:27:49 +0100

    serializeObject disclaimer

--- src/helpers/serializeObject.ts
+++ src/helpers/serializeObject.ts
@@ -1,12 +1,12 @@
 import { PlainObject } from "../types";
 
 /**
- * Serialize an object to a string, sorting the keys
+ * Serialize plain object to a deterministic string,
+ * for nested objects use [json-stable-stringify](https://www.npmjs.com/package/json-stable-stringify)
  *
  * @example
  * serializeObject({ b: 1, a: 2 }) // '{"a":1,"b":2}'
  */
-
-export const serializeObject = (obj: PlainObject) => {
+export const serializeObject = <T extends PlainObject>(obj: T) => {
   return JSON.stringify(obj, Object.keys(obj).sort());
 };
