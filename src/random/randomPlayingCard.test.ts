5d61f3d4f66f7454e2366ab2e80fc7f65987771d
Orlando <orlando.groppo@gmail.com>
2023-08-14 18:05:00 +0100

    remove non scoped files

--- src/random/randomPlayingCard.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "@jest/globals";
-import { randomBlackJackDeck } from "./randomPlayingCard";
-
-describe(`randomParagraph`, () => {
-  it(`no args`, () => {
-    expect(randomBlackJackDeck().length).toBe(52);
-  });
-});
