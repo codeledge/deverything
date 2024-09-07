bffc54f50e3b8f32d0f7432b71017124b5fc1e49
Orlando <orlando.groppo@gmail.com>
2024-09-07 19:52:52 +0100

    fix test

--- src/helpers/first.test.ts
+++ src/helpers/first.test.ts
@@ -2,10 +2,6 @@ import { describe, expect, test } from "@jest/globals";
 import { first } from "./first";
 
 describe("first", () => {
-  test("no arg", async () => {
-    expect(first()).toBeUndefined();
-  });
-
   test("args", async () => {
     expect(first([])).toBe(undefined);
     expect(first([0])).toBe(0);
