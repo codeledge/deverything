c3081b01c99db9b03c1c67dc0bf82ec84e7eece9
Orlando <orlando.groppo@gmail.com>
2025-02-17 16:49:24 +0000

    jsdocs

--- src/helpers/seriesAsync.ts
+++ src/helpers/seriesAsync.ts
@@ -9,8 +9,8 @@ type SeriesResult<T extends readonly unknown[]> = {
  * @description Run a series of (async) functions in order and return the results
  * @example
  * const results = await seriesAsync([
- *  Promise.resolve(1),
- *  sleep(100).then(() => 2),
+ *  () => Promise.resolve(1),
+ *  () => sleep(100).then(() => 2),
  *  () => Promise.resolve(3),
  *  async () => 4,
  * ]); => [1, 2, 3, 4]
